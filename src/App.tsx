26d89bcb612859d1617b6522d11a1fcb0f914326
Adib Moghimi <adib1991moghimi@gmail.com>
2026-07-20 17:14:46 +0330

    fix(src/components/Counter.tsx): Connecting the Counter component and our the Redux store.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
 import Counter from "./components/Counter";
 
-// Store
-interface CounterState {
-  value: number;
-}
-
-interface UserState {
-  isSignedIn: boolean;
-}
-
-// Actions
-const incrementByAmount = { type: "INCREMENT", payload: 10 };
-const decrement = { type: "DECREMENT" };
-
-// Reducers
-
 function App() {
   return (
     <div>
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../state/store";
+
 function Counter() {
-  return <div>Counter</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return <div></div>;
 }
 export default Counter;
